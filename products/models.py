0b6dd3bfd855a07aabd44fc6edeeb3e0a46faf81
dheeraj-ecommerce <drathore598@gmail.com>
2021-10-04 22:32:15 +0530

    add updated fixtures

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 
 from ecommerce.utils import unique_slug_generator
 
-# Create your models here.
+# Create your models here.yyy
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
